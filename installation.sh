189bbe5e6b30fabd6667ae269d5045ae3149995f
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2022-05-18 14:45:56 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo apt update
-sudo apt install ruby-full
-sudo apt install wget
+sudo apt-get install ruby-full -y
+sudo apt-get install wget -y
 cd /home/ubuntu
 sudo wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
+service codedeploy-agent start
+rm install
